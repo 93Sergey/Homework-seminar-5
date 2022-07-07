981c53475a4f008b2dec37b712f706f671f0c264
Sergey <sergey.malygin.93@mail.ru>
2022-07-07 15:50:31 +0300

    add task 38 "Массив вещественных чисел и разница между максимальным и минимальным значением"

---
+++ task38/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double[] arrayRealNumbers = new double[4];
+
+for (int i = 0; i < arrayRealNumbers.Length; i++)
+{
+    arrayRealNumbers[i] = new Random().NextDouble() * 5;
+    arrayRealNumbers[i] = Math.Round(arrayRealNumbers[i], 2);
+}
+double maxNumbers = arrayRealNumbers[0];
+double minNumbers = arrayRealNumbers[0];
+for (int i = 0; i < arrayRealNumbers.Length; i++)
+    if (maxNumbers < arrayRealNumbers[i])
+    {
+        maxNumbers = arrayRealNumbers[i];
+    }
+    else if (minNumbers > arrayRealNumbers[i])
+    {
+        minNumbers = arrayRealNumbers[i];
+    }
+
+double difference = maxNumbers - minNumbers;
+System.Console.WriteLine("[" + string.Join(", ", arrayRealNumbers) + "]" + " -> " + Math.Round(difference,2));
